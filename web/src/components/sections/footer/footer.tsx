6eb68e0b2f64b11b0294aac2434d675945381335
Svemat01 <jakob.helgesson@gmail.com>
2022-11-05 00:28:58 +0100

    Remove unnecessary overwritten margin

--- web/src/components/sections/footer/footer.tsx
+++ web/src/components/sections/footer/footer.tsx
@@ -3,7 +3,7 @@ import { GitHub, Twitter } from 'react-feather';
 
 export const Footer: FC = () => {
     return (
-        <div className="w-full flex bg-white justify-center items-center p-6 mt-2 border-2 border-solid border-t-neutral-300 border-r-0 border-l-0 border-b-0 mt-auto">
+        <div className="w-full flex bg-white justify-center items-center p-6 border-2 border-solid border-t-neutral-300 border-r-0 border-l-0 border-b-0 mt-auto">
             <div className="flex flex-col sm:flex-row gap-2 justify-between items-center w-full max-w-[800px]">
                 <div className="flex gap-6">
                     <a
