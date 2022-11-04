0b41dc5e536714a82963c289a62b9a6250f24455
Robiot <me@robiot.dev>
2022-11-05 01:29:00 +0200

    Fix container

--- web/src/components/Container.tsx
+++ web/src/components/Container.tsx
@@ -4,7 +4,7 @@ const Container: FC<{
     children: ReactNode;
 }> = (properties) => {
     return (
-        <div className="w-full px-4">
+        <div className="h-full flex w-full px-4">
             <div className="max-w-xl mx-auto text-center flex flex-col items-center gap-4">
                 {properties.children}
             </div>
--- web/src/components/sections/cta/cta.tsx
+++ web/src/components/sections/cta/cta.tsx
@@ -1,14 +1,9 @@
+import Container from '@components/Container';
 import { FC } from 'react';
 
 export const CTASection: FC = () => {
     return (
         <div className="w-full h-[80vh] bg-ens-gradient-primary relative">
-            {/* <img
-                className="h-96 absolute bottom-0 left-0 w-full"
-                src={wave}
-                alt="waves"
-            ></img> */}
-
             <svg
                 className="h-20 xl:h-60 absolute bottom-0 left-0 w-full"
                 width="100%"
@@ -25,26 +20,36 @@ export const CTASection: FC = () => {
                 />
             </svg>
 
-            <div className="flex justify-center items-center h-full flex-col gap-3">
-                <h1 className="text-white font-bold text-6xl">vrfd.eth</h1>
-                <span className="text-white">Decentralized Blue Checkmark</span>
+            <Container>
+                <div className="flex justify-center items-center h-full flex-col gap-3">
+                    <h1 className="text-white font-bold text-6xl">vrfd.eth</h1>
+                    <span className="text-white">
+                        Decentralized Blue Checkmark
+                    </span>
 
-                <div className="mt-4 flex rounded-xl bg-white w-full max-w-md">
-                    <input
-                        name="Ens Name"
-                        placeholder="v3x.eth"
-                        className="h-full w-full px-5 py-4 bg-transparent"
-                    />
-                    <button className="bg-black text-white px-7 rounded-xl">
-                        Verify
-                    </button>
-                </div>
+                    <form method="GET" action="/verify">
+                        <div className="mt-4 flex rounded-xl bg-white w-full max-w-md">
+                            <input
+                                id="name"
+                                name="Ens Name"
+                                placeholder="v3x.eth"
+                                className="h-full w-full px-5 py-4 bg-transparent"
+                            />
+                            <button
+                                type="submit"
+                                className="bg-black text-white px-7 rounded-xl"
+                            >
+                                Verify
+                            </button>
+                        </div>
+                    </form>
 
-                <div className="mt-4 flex flex-col text-center text-white gap-1">
-                    <span>type a name</span>
-                    <span>check the status</span>
+                    <div className="mt-4 flex flex-col text-center text-white gap-1">
+                        <span>type a name</span>
+                        <span>check the status</span>
+                    </div>
                 </div>
-            </div>
+            </Container>
         </div>
     );
 };
