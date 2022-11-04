08fe4335b9828a3b61e1c87cc14bcf03f4a5e5b5
Robiot <me@robiot.dev>
2022-11-05 00:20:11 +0200

    Eslint try fix

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
+ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
