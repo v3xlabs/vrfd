3194de645ce1e17e4e3649495748503fd90ef235
Lucemans <luc@lucemans.nl>
2022-11-04 23:08:49 +0100

    Response Padding About

--- web/src/components/sections/about/about.tsx
+++ web/src/components/sections/about/about.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 export const AboutSection: FC = () => {
   return (
-    <div className="w-full">
+    <div className="w-full px-4">
       <div className="max-w-xl mx-auto text-center flex flex-col gap-4">
         <div className="text-4xl font-bold text-transparent bg-clip-text bg-ens-gradient-primary">
           About
