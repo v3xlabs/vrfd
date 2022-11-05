c0edb2bd9fa5ddb6ea56f304b048d85bc07c7560
Robiot <me@robiot.dev>
2022-11-05 11:41:13 +0200

    Navbar justify center on mobile

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 export const Navbar: FC = () => {
     return (
         <nav className="z-50 w-full h-16 absolute top-0 left-0 px-8">
-            <div className="h-full flex items-center justify-end gap-4">
+            <div className="h-full flex items-center justify-center md:justify-end gap-4">
                 <a
                     className="text-white hover:underline cursor-pointer"
                     href="/"
