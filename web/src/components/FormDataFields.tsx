4c89ef2f3cc68ffdad51a18097d488e241e20105
Robiot <me@robiot.dev>
2022-11-06 17:22:47 +0200

    Fix FormDataFields with undefined

--- web/src/components/FormDataFields.tsx
+++ web/src/components/FormDataFields.tsx
@@ -23,79 +23,87 @@ export const FormDataFields: FC<{
         <form className="flex flex-col gap-4 mt-4" onSubmit={onSubmit}>
             {/* Legal Name, Twitter, Telegram, Website, Discord, Trademark, Other */}
             {JSON.stringify(hideThese)}
-            {!hideThese?.legalName && (
-                <Input
-                    label="Legal Name"
-                    placeholder="Enter your legal name"
-                    readOnly={readonly}
-                    {...register('legalName')}
-                />
-            )}
-            {!hideThese?.twitter && (
-                <div className="flex flex-col md:flex-row gap-1 md:gap-2 items-end">
+            {hideThese?.legalName == undefined ||
+                (!hideThese?.legalName && (
                     <Input
-                        label="Twitter"
-                        placeholder="Enter your Twitter handle"
+                        label="Legal Name"
+                        placeholder="Enter your legal name"
                         readOnly={readonly}
-                        {...register('twitter')}
+                        {...register('legalName')}
                     />
-                    {!readonly && (
-                        <Button className="md:m-1 h-14 md:!w-32">Link</Button>
-                    )}
-                </div>
-            )}
-
-            {!hideThese?.telegram && (
-                <Input
-                    label="Telegram"
-                    placeholder="Enter your Telegram handle"
-                    readOnly={readonly}
-                    {...register('telegram')}
-                />
-            )}
-
-            {!hideThese?.website && (
-                <Input
-                    label="Website"
-                    placeholder="Enter your website"
-                    readOnly={readonly}
-                    {...register('website')}
-                />
-            )}
-
-            {!hideThese?.discord && (
-                <div className="flex flex-col md:flex-row gap-1 md:gap-2 items-end">
+                ))}
+            {hideThese?.telegram == undefined ||
+                (!hideThese?.twitter && (
+                    <div className="flex flex-col md:flex-row gap-1 md:gap-2 items-end">
+                        <Input
+                            label="Twitter"
+                            placeholder="Enter your Twitter handle"
+                            readOnly={readonly}
+                            {...register('twitter')}
+                        />
+                        {!readonly && (
+                            <Button className="md:m-1 h-14 md:!w-32">
+                                Link
+                            </Button>
+                        )}
+                    </div>
+                ))}
+            {hideThese?.telegram == undefined ||
+                (!hideThese?.telegram && (
                     <Input
-                        label="Discord"
-                        placeholder="Enter your Discord handle"
+                        label="Telegram"
+                        placeholder="Enter your Telegram handle"
                         readOnly={readonly}
-                        {...register('discord')}
+                        {...register('telegram')}
                     />
+                ))}
+            {hideThese?.website == undefined ||
+                (!hideThese?.website && (
+                    <Input
+                        label="Website"
+                        placeholder="Enter your website"
+                        readOnly={readonly}
+                        {...register('website')}
+                    />
+                ))}
+            {hideThese?.discord == undefined ||
+                (!hideThese?.discord && (
+                    <div className="flex flex-col md:flex-row gap-1 md:gap-2 items-end">
+                        <Input
+                            label="Discord"
+                            placeholder="Enter your Discord handle"
+                            readOnly={readonly}
+                            {...register('discord')}
+                        />
 
-                    {!readonly && (
-                        <Button className="md:m-1 h-14 md:!w-32">Link</Button>
-                    )}
-                </div>
-            )}
+                        {!readonly && (
+                            <Button className="md:m-1 h-14 md:!w-32">
+                                Link
+                            </Button>
+                        )}
+                    </div>
+                ))}
 
-            {!hideThese?.trademark && (
-                <Input
-                    label="Trademark"
-                    placeholder="Enter your trademark"
-                    readOnly={readonly}
-                    {...register('trademark')}
-                />
-            )}
+            {hideThese?.trademark == undefined ||
+                (!hideThese?.trademark && (
+                    <Input
+                        label="Trademark"
+                        placeholder="Enter your trademark"
+                        readOnly={readonly}
+                        {...register('trademark')}
+                    />
+                ))}
 
-            {!hideThese?.other && (
-                <Textarea
-                    label="Other"
-                    placeholder="Enter other information"
-                    className="h-52"
-                    readOnly={readonly}
-                    {...register('other')}
-                />
-            )}
+            {hideThese?.other == undefined ||
+                (!hideThese?.other && (
+                    <Textarea
+                        label="Other"
+                        placeholder="Enter other information"
+                        className="h-52"
+                        readOnly={readonly}
+                        {...register('other')}
+                    />
+                ))}
             {children}
         </form>
     );
